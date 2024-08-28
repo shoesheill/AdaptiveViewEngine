0198dd2e54da1663686ecf6c70a033edccc91595
Sushil Shrestha <sushil@sageflick.com>
2024-08-28 13:39:54 +0545

    update(Controller): localization on controller
    - create Localization for Controllers
    - implement localization on Home Controller

--- AdaptiveViewEngine/Controllers/HomeController.cs
+++ AdaptiveViewEngine/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
 using AdaptiveViewEngine.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
 
 namespace AdaptiveViewEngine.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IStringLocalizer<HomeController> localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
 
         public IActionResult Index()
         {
+            var message = _localizer["Welcome"];
             return View();
         }
 
