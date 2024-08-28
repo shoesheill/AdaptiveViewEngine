2c15d1e2c2933f48a6863b112d5c2e2a4a138593
Sushil Shrestha <sushil@sageflick.com>
2024-08-28 11:17:19 +0545

    update(config): add Localization configuration on program.cs
    - add viewlocalization
    - add data anotationlocationzation
    - add localization resource path
    - add request configurations , available cultures and default culture

--- AdaptiveViewEngine/Program.cs
+++ AdaptiveViewEngine/Program.cs
@@ -1,15 +1,33 @@
 using AdaptiveViewEngine.Helper;
 using AdaptiveViewEngine.Middleware;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization()
     .AddRazorOptions(options =>
     {
         options.ViewLocationExpanders.Add(new DomainViewLocationExpander());
     });
+
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+// Configure localization options
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { "en-US", "fr-FR" };
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
+    options.SupportedUICultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
+});
 
 var app = builder.Build();
 app.UseMiddleware<DomainMiddleware>();
@@ -28,6 +46,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseRequestLocalization();
 
 app.MapControllerRoute(
     name: "default",
